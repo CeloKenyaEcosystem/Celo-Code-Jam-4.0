30627e3d2a2b6936d5989d11a0477e16021ca549
RonexLemon <ronexondimu@gmail.com>
2024-02-16 18:12:43 +0300

    add account spinner

--- StreamSway/client/stream/src/components/spinner.jsx
+++ StreamSway/client/stream/src/components/spinner.jsx
@@ -1,12 +1,13 @@
 
-export const Spinner =({size})=>{
+export const Spinner =({size,text="Loading..."})=>{
     return(
         <div className="w-[50%]  flex-center h-10">
     <div
       style={{ width: `${size}px`, height: `${size}px` }}
       className="animate-spin">
-      <div className="h-full w-full border-4 border-t-purple-500
-       border-b-purple-700 rounded-[50%]">
+      <div className="h-full w-full border-4 border-t-green-500
+       border-b-green-700 rounded-[50%]">
+        
       </div>
     </div>
   </div>
